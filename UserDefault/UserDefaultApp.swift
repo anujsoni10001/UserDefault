13fae51b6543f7464dbaf67b72498baa68d848a7
Anuj Soni <anujsoni10001@gmail.com>
2022-03-03 11:24:07 +0530

    Initial Commit

---
+++ UserDefault/UserDefaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultApp.swift
+//  UserDefault
+//
+//  Created by Anuj Soni on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
